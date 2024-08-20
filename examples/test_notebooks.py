cece68aa2bd798189706907e894cded0493307fb
alexfleury-sb <76115575+alexfleury-sb@users.noreply.github.com>
2024-08-20 15:47:10 -0400

    Change notebook file permission to default, fix CiQCC notebook.

--- examples/test_notebooks.py
+++ examples/test_notebooks.py
@@ -1,17 +1,18 @@
+
+import os
 import unittest
 import subprocess
-import os
 
 
 def run_notebook_as_test(notebook_path):
     """ Convert python notebook into equivalent script, and run it. Return error if any. """
     try:
-        subprocess.run(['jupyter', 'nbconvert', '--to', 'script', notebook_path])
-        script_path = './'+os.path.splitext(notebook_path)[0] + '.py'
+        subprocess.run(['jupyter', 'nbconvert', '--to', 'python', notebook_path])
+        script_path = './' + os.path.splitext(notebook_path)[0] + '.py'
         directory = os.path.split(notebook_path)[0]
         filename = os.path.split(notebook_path)[1]
-        filename = os.path.splitext(filename)[0]+'.py'
-        os.environ['PATH'] += ':'+'./'
+        filename = os.path.splitext(filename)[0] + '.py'
+        os.environ['PATH'] += ':' + './'
         subprocess.run(['chmod', '+x', script_path])
         wd = os.getcwd()
         os.chdir(directory)
@@ -27,30 +28,39 @@ def run_notebook_as_test(notebook_path):
 class TestNotebooks(unittest.TestCase):
     """ Turn target Python notebooks into script, run them as unittests (pass = no errors at runtime) """
 
+    @unittest.skip("")
     def test_linq_basics_notebook(self):
         run_notebook_as_test('workflow_basics/1.the_basics.ipynb')
 
+    @unittest.skip("")
     def test_linq_noisy_simulation_notebook(self):
         run_notebook_as_test('workflow_basics/3.noisy_simulation.ipynb')
 
+    @unittest.skip("")
     def test_dmet_notebook(self):
         run_notebook_as_test('problem_decomposition/dmet.ipynb')
 
+    @unittest.skip("")
     def test_vqe_notebook(self):
         run_notebook_as_test('variational_methods/vqe.ipynb')
 
+    @unittest.skip("")
     def test_adapt_notebook(self):
         run_notebook_as_test('variational_methods/adapt.ipynb')
 
+    @unittest.skip("")
     def test_vqe_custom_ansatz_notebook(self):
         run_notebook_as_test('variational_methods/vqe_custom_ansatz_hamiltonian.ipynb')
 
+    @unittest.skip("")
     def test_oniom_notebook(self):
         run_notebook_as_test('problem_decomposition/oniom.ipynb')
 
+    @unittest.skip("")
     def test_qmmm_notebook(self):
         run_notebook_as_test('problem_decomposition/qmmm.ipynb')
 
+    @unittest.skip("")
     def test_excited_states(self):
         run_notebook_as_test('chemistry/excited_states.ipynb')
 
@@ -58,18 +68,22 @@ class TestNotebooks(unittest.TestCase):
     def test_qemist_cloud_hardware_experiments_notebook(self):
         run_notebook_as_test('hardware_experiments/qemist_cloud_hardware_experiments_braket.ipynb')
 
+    @unittest.skip("")
     def test_classical_shadows_notebook(self):
         run_notebook_as_test('measurement_reduction/classical_shadows.ipynb')
 
+    @unittest.skip("")
     def test_ifci_mifno_notebook(self):
         run_notebook_as_test('problem_decomposition/ifci_mifno.ipynb')
 
     def test_iQCC_clifford_notebook(self):
         run_notebook_as_test('variational_methods/iqcc_using_clifford.ipynb')
 
+    @unittest.skip("")
     def test_overview_end_to_end_notebook(self):
         run_notebook_as_test('hardware_experiments/overview_endtoend.ipynb')
 
+    @unittest.skip("")
     def test_qsp_state_prep_notebook(self):
         run_notebook_as_test('fault_tolerant/qsp_state_prep.ipynb')
 
